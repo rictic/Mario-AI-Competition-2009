dce33485cbc3922afaffcd278b190e820247bf31
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-04-30 00:31:16 +0000

    mv Utils

--- src/Python/iPyMario/src/iPyMario.py
+++ src/Python/iPyMario/src/iPyMario.py
@@ -1,16 +1,9 @@
-__author__="Sergey Karakovskiy, sergey at idsia dot chosen"
+__author__="Sergey Karakovskiy, sergey at idsia dot ch"
 __date__ ="$Apr 30, 2009 1:46:32 AM$"
 import sys
 
 import numpy
 import TCP
-
-if __name__ == "__main__":
-    print numpy.random.randint(34, 39)
-    print "Hello";
-    print TCP.__name__
-    print TCP.__author__
-
 
 if __name__ == "__main__":
     print "Hello World";
