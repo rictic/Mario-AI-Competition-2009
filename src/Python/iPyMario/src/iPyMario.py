89c61704c8c56035ceb90726999e025669d54153
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-05-02 19:44:23 +0000

    bug fixes, improvements, prepare script to run using sys.args. example: python iPyMario.py --agent ForwardAgent --port 4242

--- src/Python/iPyMario/src/iPyMario.py
+++ src/Python/iPyMario/src/iPyMario.py
@@ -1,3 +1,4 @@
+import getopt
 __author__="Sergey Karakovskiy, sergey at idsia dot ch"
 __date__ ="$Apr 30, 2009 1:46:32 AM$"
 from AI.Environment import Environment
@@ -6,13 +7,35 @@ from AI import ForwardRandomAgent
 from AI import ForwardAgent
 from Network import TCPClientAgent
 
-if __name__ == "__main__":
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "pa", ["port=", "agent="])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
     HOST = 'localhost'
     PORT = 4242
-#    fra = ForwardRandomAgent()
-#    agent = TCPClientAgent(fra, HOST, PORT)
-    fa = ForwardAgent(Environment())
-    agent = TCPClientAgent(fa, HOST, PORT)
+    agentName = "ForwardAgent"
+    actingAgent = None
+
+    for o, a in opts:
+        if o == "--port":
+            PORT = int(a)
+        elif o == "--agent":
+            agentName = a
+        else:
+            assert False, "unhandled option"
+    if agentName == "ForwardAgent":
+        actingAgent = ForwardAgent(Environment())
+    elif agentName == "ForwardRandomAgent":
+        actingAgent = ForwardRandomAgent()
+    else:
+        assert  False, "unknown Agent"
+
+    agent = TCPClientAgent(actingAgent, HOST, PORT)
     while True:
         agent.getObservation()
         if not agent.isConnected():
@@ -22,5 +45,13 @@ if __name__ == "__main__":
         agent.produceAction()
 
     sys.exit(0)
+
+def usage ():
+    print "python iPyMario.py [-h][-p port][-a AgentName]"
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
 else:
-    print "Better run this file:"
+    print "This is file to be run rather than imported."
+
