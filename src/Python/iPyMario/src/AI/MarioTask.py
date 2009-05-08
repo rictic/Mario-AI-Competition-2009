066bf90499b37839804a4c3ac2637bda5049aa0c
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-05-08 19:24:02 +0000

    MarioTask added

---
+++ src/Python/iPyMario/src/AI/MarioTask.py
@@ -0,0 +1,10 @@
+__author__="Sergey Karakovskiy, sergey at idsia fullstop ch"
+__date__ ="$May 7, 2009 12:47:18 PM$"
+
+if __name__ != "__main__":
+    print "Loading %s ..." % __name__;
+
+class MarioTaskEpisodicTask:
+    """
+    Documentation
+    """
