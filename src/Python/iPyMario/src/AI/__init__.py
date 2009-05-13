defabdffd517266cd702ca9aca0fed62e47a74b6
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-05-13 06:53:10 +0000

    tasks, EpisodicTask, MarioTask

--- src/Python/iPyMario/src/AI/__init__.py
+++ src/Python/iPyMario/src/AI/__init__.py
@@ -1,5 +1,5 @@
-__author__="Duke"
+__author__="Sergey Karakovskiy, sergey at idsia fullstop ch"
 __date__ ="$Apr 30, 2009 2:39:06 PM$"
 
-from AI.ForwardRandomAgent import ForwardRandomAgent
-from AI.ForwardAgent import ForwardAgent
+from AI.agents.ForwardRandomAgent import ForwardRandomAgent
+from AI.agents.ForwardAgent import ForwardAgent
