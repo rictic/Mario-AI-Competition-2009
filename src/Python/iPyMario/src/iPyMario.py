9ff2534e116b321181f650fb91fe3ee16504219d
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-05-13 06:59:59 +0000

    simple example with new improved API introduced

--- src/Python/iPyMario/src/iPyMario.py
+++ src/Python/iPyMario/src/iPyMario.py
@@ -1,60 +1,18 @@
-import getopt
 __author__="Sergey Karakovskiy, sergey at idsia dot ch"
 __date__ ="$Apr 30, 2009 1:46:32 AM$"
-from AI.Environment import Environment
+
 import sys
-from AI import ForwardRandomAgent
-from AI import ForwardAgent
-from Network import TCPClientAgent
+from AI.environments.MarioEnvironment import MarioEnvironment
+from AI.experiments.EpisodicExperiment import EpisodicExperiment
+from AI.tasks.MarioTask import MarioTask
+from Utils.CmdLineOptions import CmdLineOptions
 
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "pa", ["port=", "agent="])
-    except getopt.GetoptError, err:
-        print str(err)
-        usage()
-        sys.exit(2)
-
-    HOST = 'localhost'
-    PORT = 4242
-    agentName = "ForwardAgent"
-    actingAgent = None
-
-    for o, a in opts:
-        if o == "--port":
-            PORT = int(a)
-        elif o == "--agent":
-            agentName = a
-        else:
-            assert False, "unhandled option"
-    if agentName == "ForwardAgent":
-        actingAgent = ForwardAgent(Environment())
-    elif agentName == "ForwardRandomAgent":
-        actingAgent = ForwardRandomAgent()
-    else:
-        assert  False, "unknown Agent"
-
-    task = MarioTask(difficulty = 4)
-    exp = EpisodicExperiment(actingAgent, task)
-    exp.doEpisodes(10)
-
-    class MarioTask(EpisodicTask):
-
-    MarioEnvironment()
-
-    agent = TCPClientAgent(actingAgent, HOST, PORT)
-    while True:
-        agent.getObservation()
-        if not agent.isConnected():
-            print "Agent was disconnected. Exiting..."
-            break
-#            sys.exit(1)
-        agent.produceAction()
-
-def usage ():
-    print "python iPyMario.py [--port port][--agent AgentName]"
-    
+    clo = CmdLineOptions(sys.argv)
+    task = MarioTask(MarioEnvironment(clo.getHost(), clo.getPort(), clo.getAgent().name))
+    exp = EpisodicExperiment(clo.getAgent(), task)
+    exp.doEpisodes(3)
 
 if __name__ == "__main__":
     main()
