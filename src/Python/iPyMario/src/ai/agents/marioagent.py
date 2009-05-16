06d1b997bcd384c6d39429a4fcbfd150195b2192
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-05-16 23:26:31 +0000

    Python

--- src/Python/iPyMario/src/ai/agents/marioagent.py
+++ src/Python/iPyMario/src/ai/agents/marioagent.py
@@ -2,6 +2,7 @@ __author__="Sergey Karakovskiy, sergey at idsia fullstop ch"
 __date__ ="$May 2, 2009 7:54:12 PM$"
 
 class MarioAgent:
+#    class MarioAgent(Agent):
     """ An agent is an entity capable of producing actions, based on previous observations.
         Generally it will also learn from experience. It can interact directly with a Task.
     """
