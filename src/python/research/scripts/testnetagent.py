60ee2cfe5ce4a0b9c53339c3a4d873dea6476b5f
Tom Schaul <tom@idsia.ch>
2009-05-25 16:28:23 +0000

    Fast Mdrnn agents fixed. Simple-mdrnn agents added.

--- src/python/research/scripts/testnetagent.py
+++ src/python/research/scripts/testnetagent.py
@@ -3,34 +3,37 @@ __author__ = "Tom Schaul"
 from pybrain.rl.experiments.episodic import EpisodicExperiment
 from tasks.mariotask import MarioTask
 from agents.networkagent import MLPMarioAgent, SimpleMLPMarioAgent
+from agents.mdrnnagent import MdrnnAgent, SimpleMdrnnAgent
+from pybrain.tools.xml.networkwriter import NetworkWriter
 
 
 def combinedScore(agent, task = None):
     """ Let the agent act on a number of levels of increasing difficulty. 
     Return the combined score."""
     if task == None:
-        task = MarioTask(agent.name)
+        task = MarioTask(agent.name, timeLimit = 20)    
     exp = EpisodicExperiment(task, agent)
     res = 0
-    for difficulty in range(12):
-        for seed in range(15):
+    for difficulty in range(5):
+        for seed in range(2):
             task.env.levelSeed = seed
             task.env.levelDifficulty = difficulty  
+            
             exp.doEpisodes(1)
             print 'Difficulty: %d, Seed: %d, Fitness: %.2f' % (difficulty, seed, task.reward)
             res += task.reward
     return res
     
 def main():
-    agent1 = SimpleMLPMarioAgent(2)
+    #agent1 = SimpleMLPMarioAgent(2)
+    #agent2 = MLPMarioAgent(4)
+    #agent1 = MdrnnAgent()
+    agent1 = SimpleMdrnnAgent()
     print agent1.name
+    NetworkWriter.writeToFile(agent1.module, "../temp/MarioNetwork-"+agent1.name+".xml")
     f = combinedScore(agent1)
     print "\nTotal:", f
     
-    agent2 = MLPMarioAgent(4)
-    print agent2.name
-    f = combinedScore(agent2)
-    print "\nTotal:", f
     
     
 if __name__ == "__main__":
