d1dd685c346e6457f78b2cc158ec86b329950980
Jan Koutnik <hkou@idsia.ch>
2009-07-27 15:14:14 +0000

    A wrapper class for one scenario evaluation called directly from Mathematica.

---
+++ src/ch/idsia/scenarios/test/EvaluateJLink.java
@@ -0,0 +1,19 @@
+package ch.idsia.scenarios.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: koutnij
+ * Date: Jul 27, 2009
+ * Time: 4:34:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JLink {
+
+    public double evaluateLargeSRN (double[][] inputs, double[][] recurrent, double[][] output) {
+        // 98 * 6
+        // 6*6
+        // 6*6
+        return 0;
+    }
+
+}
