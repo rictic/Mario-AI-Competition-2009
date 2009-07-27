619f8e6cbc77d5ea8dcf7c16c98c51eea1eca818
Jan Koutnik <hkou@idsia.ch>
2009-07-27 15:43:48 +0000

    Just added a function that returns dimensions of the network to be generated.

--- src/ch/idsia/scenarios/test/EvaluateJLink.java
+++ src/ch/idsia/scenarios/test/EvaluateJLink.java
@@ -7,7 +7,13 @@ package ch.idsia.scenarios.test;
  * Time: 4:34:37 PM
  * To change this template use File | Settings | File Templates.
  */
-public class JLink {
+public class EvaluateJLink {
+
+
+    /** returns {in, rec, out} array. Just to make math and java codes fully independent. */
+    public static int[] getDimension() {
+        return new int[]{98, 6, 6};
+    }
 
     public double evaluateLargeSRN (double[][] inputs, double[][] recurrent, double[][] output) {
         // 98 * 6
