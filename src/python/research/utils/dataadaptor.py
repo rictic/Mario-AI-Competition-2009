5c82acf7b17a087c69a5760b05d99d14fb97d907
Sergey Karakovskiy <sergey.karakovskiy@gmail.com>
2009-08-07 13:30:41 +0000

    fast tcp available

--- src/python/research/utils/dataadaptor.py
+++ src/python/research/utils/dataadaptor.py
@@ -3,6 +3,39 @@ __date__ = "$Apr 30, 2009 1:53:54 PM$"
 
 import numpy
     
+powsof2 = (1, 2, 4, 8, 16, 32, 64, 128, 256)
+
+def decode(estate):
+    """
+    decodes the encoded state estate, which is a string of 61 chars
+    """
+    levelScene = numpy.empty(shape = (22, 22), dtype = numpy.int)
+    enemies = numpy.empty(shape = (22, 22), dtype = numpy.int)
+    dstate = numpy.empty(shape = (22, 22), dtype = numpy.int)
+    for i in range(22):
+        for j in range(22):
+            dstate[i, j] = 2
+    row = 0
+    col = 0
+    totalBitsDecoded = 0
+    for i in range(len(estate)):
+        cur_char = estate[i]
+        for j in range(8):
+            totalBitsDecoded += 1
+            if (col > 11 * 2 - 1):
+                row += 1
+                col = 0
+            if (powsof2[j] & ord(cur_char) != 0):
+                    dstate[row, col] = 1
+            else:
+                dstate[row, col] = 0
+            col += 1
+            if (totalBitsDecoded == 484):
+                break
+    print "\ntotalBitsDecoded = ", totalBitsDecoded
+    return dstate;
+
+
 def extractObservation(data):
     """
      parse the array of strings and return array 22 by 22 of doubles
@@ -22,12 +55,20 @@ def extractObservation(data):
     elif(data[0] == 'O'):
         mayMarioJump = (data[1] == 'true')
         isMarioOnGround = (data[2] == 'true')
-        assert len(data) == obsLength, "Error in data size given %d! Required: %d \n data: %s " % (len(data), obsLength, data)
+#        assert len(data) == obsLength, "Error in data size given %d! Required: %d \n data: %s " % (len(data), obsLength, data)
         k = 0
         for i in range(22):
             for j in range(22):
                 levelScene[i, j] = int(data[k + 3])
                 k += 1
+        return (mayMarioJump, isMarioOnGround, levelScene)
+    elif(data[0][0] == 'E'): #Encoded observation, fastTCP mode, have to be decoded
+#        assert len(data) == eobsLength
+        print "E",
+        mayMarioJump = (data[0][1] == '1')
+        isMarioOnGround = (data[0][2] == '1')
+        levelScene = decode(data[0][3:64])
+        enemies = decode(data[0][64:])
         return (mayMarioJump, isMarioOnGround, levelScene)
     else:
         raise "Wrong format or corrupted observation..."
