ea7ecfdcc9b5359dda2669d7ccdf7ae5fb0947f9
Peter Burns <peter@metaweb.com>
2009-08-08 15:19:23 -0700

    Reverted Play.java to be HumanKeyboardAgent by default

--- src/ch/idsia/scenarios/Play.java
+++ src/ch/idsia/scenarios/Play.java
@@ -3,6 +3,7 @@ package ch.idsia.scenarios;
 import ch.idsia.ai.agents.Agent;
 import ch.idsia.ai.agents.RegisterableAgent;
 import ch.idsia.ai.agents.ai.*;
+import ch.idsia.ai.agents.human.HumanKeyboardAgent;
 import ch.idsia.ai.tasks.ProgressTask;
 import ch.idsia.ai.tasks.Task;
 import ch.idsia.tools.CmdLineOptions;
@@ -18,7 +19,7 @@ import ch.idsia.utils.ArrayUtils;
 public class Play {
 
     public static void main(String[] args) {
-        Agent controller = new HardcodedAgent();
+        Agent controller = new HumanKeyboardAgent();
         if (args.length > 0) {
             controller = RegisterableAgent.load (args[0]);
             RegisterableAgent.registerAgent (controller);
