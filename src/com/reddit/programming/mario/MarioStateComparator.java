24013ee27ffc39a6771de0afc08d296530e26c29
Peter Burns <peter@metaweb.com>
2009-08-09 13:28:13 -0700

    Minor change to MarioStateComparator

--- src/com/reddit/programming/mario/MarioStateComparator.java
+++ src/com/reddit/programming/mario/MarioStateComparator.java
@@ -2,9 +2,7 @@ package com.reddit.programming.mario;
 
 import java.util.Comparator;
 
-public class MarioStateComparator implements Comparator<MarioState>
-{
-  @Override
+public class MarioStateComparator implements Comparator<MarioState> {
   public int compare(MarioState a, MarioState b) {
     if(a.cost < b.cost) return -1;
     if(a.cost > b.cost) return 1;
