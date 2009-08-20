edab3c1b555b583dd5e4bc5ea214f23c55b2a5b8
Bart van der Werf <bluelive@gmail.com>
2009-08-20 21:42:18 +0200

    Ugly ass hillclimber with far to much random to be usefull.

--- src/com/reddit/programming/mario/Tunables.java
+++ src/com/reddit/programming/mario/Tunables.java
@@ -2,13 +2,13 @@ package com.reddit.programming.mario;
 
 
 public class Tunables {
-	static float FactorA = 1;
-	static float FactorB = 1;
-	static float FactorC = 1;
-	static float DeathPenalty = 1000;
-	static float DeathPenaltyWeakening = 4;
-	static float GIncrement = 1;
-	static float DeadCost = Float.POSITIVE_INFINITY;
-	static float ChasmPenalty = Float.POSITIVE_INFINITY;
-	static float FeetOnTheGroundBonus = 0;
+	public static float FactorA = 1.33f;
+	public static float FactorB = 1;
+	public static float FactorC = 1;
+	public static float DeathPenalty = 1000;
+	public static float DeathPenaltyWeakening = 4;
+	public static float GIncrement = 1;
+	public static float DeadCost = 1e10f;
+	public static float ChasmPenalty = 1e10f;
+	public static float FeetOnTheGroundBonus = 0;
 }
--- src/com/reddit/programming/mario/tweaker/TweakRunner.java
+++ src/com/reddit/programming/mario/tweaker/TweakRunner.java
@@ -14,12 +14,76 @@ import ch.idsia.mario.engine.GlobalOptions;
 import ch.idsia.tools.CmdLineOptions;
 import ch.idsia.tools.EvaluationOptions;
 import ch.idsia.utils.ArrayUtils;
+import java.util.Random;
 
 public class TweakRunner {
 	
 	public static void main(String[] args) {
+		float[] best = new float[9];
+		best[0] = Tunables.FactorA;
+		best[1] = Tunables.FactorB;
+		best[2] = Tunables.FactorC;
+		best[3] = Tunables.DeathPenalty;
+		best[4] = Tunables.DeathPenaltyWeakening;
+		best[5] = Tunables.GIncrement;
+		best[6] = Tunables.DeadCost;
+		best[7] = Tunables.ChasmPenalty;
+		best[8] = Tunables.FeetOnTheGroundBonus;
+		
+		float[] settings = new float[9];
+		for (int i = 0; i<best.length; ++i)
+			settings[i] = best[i];
+
+		float bestScore = -1e10f;		
+			
+		Random rnd = new Random();
+		boolean flip = false;
 		for (int i =0 ; i< 1000; ++i)
-			System.out.println(DoRun());
+		{
+			Tunables.FactorA = settings[0];
+			Tunables.FactorB = settings[1];
+			Tunables.FactorC = settings[2];
+			Tunables.DeathPenalty = settings[3];
+			Tunables.DeathPenaltyWeakening = settings[4];
+			Tunables.GIncrement = settings[5];
+			Tunables.DeadCost = settings[6];
+			Tunables.ChasmPenalty = settings[7];
+			Tunables.FeetOnTheGroundBonus = settings[8];
+			float score = DoRun();
+			if (score > bestScore)
+			{
+				flip = false;
+				bestScore = score;
+				for (int j = 0; j<best.length; ++j)
+					best[j] = settings[j];
+				System.out.println(score+":"+ArrayUtils.toString(best));
+			}
+			else if ((score == bestScore)&&!flip)
+			{
+				System.out.println("=");
+				for (int j = 0; j<best.length; ++j)
+					settings[j] = settings[j] + (best[j] - settings[j]);
+			}
+			else
+			{
+				if (flip)
+				{
+					flip = false;
+					System.out.println(".");
+					for (int j = 0; j<best.length; ++j)
+						settings[j] = best[j];
+					int j = rnd.nextInt(best.length);
+					settings[j] *= 1 + 0.02*(0.5 - rnd.nextFloat());
+				}
+				else
+				{
+					flip = true;
+					System.out.println("-");
+					for (int j = 0; j<best.length; ++j)
+						settings[j] = settings[j] - (best[j] - settings[j]);
+				}
+			}
+		}
 	}
 	
 	private static float DoRun()
@@ -38,7 +102,7 @@ public class TweakRunner {
 		EvaluationOptions options = new CmdLineOptions(new String[0]);
 		options.setAgent(controller);
 		Task task = new ProgressTask(options);
-		options.setMaxFPS(false);
+		options.setMaxFPS(true);
 		options.setVisualization(false);
 		options.setMaxAttempts(1);
 		options.setMatlabFileName("");
